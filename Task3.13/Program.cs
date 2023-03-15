e79144dbb3273006e6b3f7e7bd67a3720bdfefc3
Dina Nevhod <Dina_Nevhod@epam.com>
2023-03-15 12:14:52 +0100

    Add project files.

---
+++ Task3.13/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task3._13
+{
+    internal class Program
+    {
+        public static void createRandomString(int Length)
+        {
+            string _allowedChars = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ";
+            Random randNum = new Random();
+            char[] chars = new char[Length];
+
+            for (int i = 0; i < Length; i++)
+            {
+                chars[i] = _allowedChars[(int)((_allowedChars.Length) * randNum.NextDouble())];
+            }
+
+            int koordinateX = randNum.Next(20, 1900);
+            int koordinateY = randNum.Next(20, 900);
+            Console.SetCursorPosition(koordinateX, koordinateY);
+            for (int i = 0; i < chars.Length; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case 1: Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        break;
+                    default: Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                }
+
+                Console.SetCursorPosition(koordinateX, koordinateY - i);
+                Console.WriteLine(chars[i]);
+            }
+            
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random rand = new Random();
+                createRandomString(rand.Next(5,20));
+            }
+        }
+    }
+}
